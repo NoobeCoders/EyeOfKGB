6a0b179a02ef8ed641d031ebc5ccbb2c4cb998ff
Rammun <russslllan@mail.ru>
2016-01-21 14:04:07 +0300

    Добавил

---
+++ Crawler/BusinessLogic/Dawnloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Dawnloader
+    {
+        public string Download(String url)
+        {
+            WebClient client;
+            string answer;
+
+            try
+            {
+                using (client = new WebClient())
+                {
+                    Byte[] pageData = client.DownloadData(url);
+
+                    answer = Encoding.ASCII.GetString(pageData);
+                }
+            }
+            catch (Exception ex)
+            {
+                answer = ex.Message;
+            }
+
+            return answer;
+        }
+    }
+}
