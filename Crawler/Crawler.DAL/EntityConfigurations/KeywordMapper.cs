6ea374a53744b57f35a566394a17c22d5508ed75
Rammun <russslllan@mail.ru>
2016-01-21 12:32:29 +0300

    Добавил флюпи апи

---
+++ Crawler/Crawler.DAL/EntityConfigurations/KeywordMapper.cs
@@ -0,0 +1,28 @@
+﻿using Crawler.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.DAL.EntityConfigurations
+{
+    public class KeywordMapper : EntityTypeConfiguration<Keyword>
+    {
+        public KeywordMapper()
+        {
+            this.ToTable("Keywords");
+
+            this.Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.True);
+            this.Property(c => c.Id).IsRequired();
+
+            this.Property(c => c.Name).HasMaxLength(2048);
+
+            this.HasRequired<Person>(c => c.Person)
+                .WithMany(c => c.Keywords)
+                .HasForeignKey(c => c.PersonId);
+        }
+    }
+}
---
+++ Crawler/Crawler.DAL/EntityConfigurations/PageMapper.cs
@@ -0,0 +1,35 @@
+﻿using Crawler.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.DAL.EntityConfigurations
+{
+    public class PageMapper : EntityTypeConfiguration<Page>
+    {
+        public PageMapper()
+        {
+            this.ToTable("Pages");
+
+            this.Property(c => c.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.True);
+            this.Property(c => c.Id).IsRequired();
+
+            this.Property(c => c.URL).HasMaxLength(2048);
+
+            this.HasMany<KeywordMapper>(c => c.Keywords)
+                .WithRequired(c => c.Person)
+                .HasForeignKey(c => c.PersonId)
+                .WillCascadeOnDelete;
+
+            this.HasMany<PersonPageRank>(c => c.PersonPageRanks)
+                .WithRequired(c => c.Page)
+                .HasForeignKey(c => c.PageId)
+                .WillCascadeOnDelete;
+        }
+        
+    }
+}
