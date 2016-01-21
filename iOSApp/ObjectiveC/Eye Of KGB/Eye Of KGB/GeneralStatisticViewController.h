e3f60f29417f863aeaec82fd63d2dc0abe4f1566
olegshamin <githuboleg@gmail.com>
2016-01-21 18:45:18 +0700

    Interface general statistic

--- iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticViewController.h
+++ iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface GeneralStatisticViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UIPickerView *sitePicker;
+@property (strong, nonatomic) NSMutableArray *sites;
 @end
--- iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticViewController.m
+++ iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticViewController.m
@@ -7,16 +7,20 @@
 //
 
 #import "GeneralStatisticViewController.h"
+#import "GeneralStatisticTableViewCell.h"
 
-@interface GeneralStatisticViewController ()
-
+@interface GeneralStatisticViewController () <UITableViewDataSource, UITableViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate>
 @end
 
 @implementation GeneralStatisticViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.sitePicker.delegate = self;
+    self.sitePicker.dataSource = self;
+    
+    self.sites = [[NSMutableArray alloc] initWithObjects: @"Lenta.ru", @"Gazeta.ru", @"RBC.ru", nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +28,43 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+//MARK: - Delegates and data sources (table)
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
 }
-*/
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    GeneralStatisticTableViewCell *cell = (GeneralStatisticTableViewCell *) [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    NSArray *names = [[NSArray alloc] initWithObjects: @"Путин", @"Медведев", @"Навальный", nil];
+    
+    NSArray *counts = [[NSArray alloc] initWithObjects: @"10500", @"5000", @"1000", nil];
+    
+    cell.labelName.text = names[indexPath.row];
+    cell.labelCount.text = counts[indexPath.row];
+    
+    return cell;
+    
+}
+
+//MARK: - Delegates and datasource (pickerview)
+
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.sites.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)thePickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return [self.sites objectAtIndex:row];
+}
+
 
 @end
