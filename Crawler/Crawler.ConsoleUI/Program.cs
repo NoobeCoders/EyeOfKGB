3578056974400fdae1340a5555df3fb890d6f0e3
Rammun <russslllan@mail.ru>
2016-01-21 15:52:33 +0300

    Добвал из нетрасируемых

---
+++ Crawler/Crawler.ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dawnloader dawnloader = new Dawnloader();
+            string text = dawnloader.Download(@"https://github.com/robots.txt");
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
