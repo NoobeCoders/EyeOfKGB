fb5a7c8adc0003f42c176e4f7c74ba34545d845d
marcheneli <marcheneli@gmail.com>
2016-01-21 10:10:29 +0300

    Add main entities

---
+++ Crawler/Crawler.Domain/Entities/Page.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Domain.Entities
+{
+    public class Page
+    {
+        public int ID { get; set; }
+        public string Url { get; set; }
+        public int SiteID { get; set; }
+        public DateTime FoundDateTime { get; set; }
+        public DateTime LastSacnDate { get; set; }
+    }
+}
---
+++ Crawler/Crawler.Domain/Entities/PersonPageRank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Domain.Entities
+{
+    public class PersonPageRank
+    {
+        public int PersonID { get; set; }
+        public int PageID { get; set; }
+        public int Rank { get; set; }
+    }
+}
