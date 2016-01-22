0b6f444939daa7fb93305f9bfbf36630c9d2b728
marcheneli <marcheneli@gmail.com>
2016-01-22 19:02:00 +0300

    Add controllers and sitemap.xml for crawler testing to webui project

---
+++ Crawler/Crawler/Controllers/TestCrawlerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crawler.Controllers
+{
+    public class TestCrawlerController : Controller
+    {
+        // GET: TestCrawler
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test1()
+        {
+            return View();
+        }
+
+        public ActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
