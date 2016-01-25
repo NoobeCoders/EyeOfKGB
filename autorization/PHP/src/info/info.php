5b7029279db13a89d3f56f5d985478d956e9d49e
vinnezor <chirkov.bat@gmail.com>
2016-01-25 18:01:26 +0300

    Авторизация версия 0.1 регистрирует пользователя, хеширует пароль, проверят наличие login в бaзе. следующее обновление - вывод администратору всех пользователей, с их ролями

---
+++ autorization/PHP/src/info/info.php
@@ -0,0 +1,7 @@
+<?php
+    // Информация для базы и соль правда она не тут должна лежать наверное
+    $host = "mysql:host=localhost;dbname=eyeofkgb";
+    $user = "vin";
+    $pass = "admin";
+    $salt = 'testingforuserpassword';
+    ?>
