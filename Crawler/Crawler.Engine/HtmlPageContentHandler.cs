1133c44a0623a89413d7d7cb5d38f95eac5385af
marcheneli <marcheneli@gmail.com>
2016-01-27 01:07:40 +0300

    Add new methods

--- Crawler/Crawler.Engine/HtmlPageContentHandler.cs
+++ Crawler/Crawler.Engine/HtmlPageContentHandler.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Crawler.Engine
@@ -22,17 +23,48 @@ namespace Crawler.Engine
 
         public override void HandleContent(Page page, string htmlContent)
         {
+            GetRank(page, htmlContent);
+            InsertNewPages(page, htmlContent);
+        }
+
+        private void GetRank(Page page, string htmlContent)
+        {
             IEnumerable<string> pagePhrases = Parser.GetPagePhrases(htmlContent);
 
             foreach (Person person in persons)
             {
                 int rank = CountRank(person.Keywords, pagePhrases);
 
-                SavePersonPageRank(person, page, rank);
+                InsertPersonPageRank(person, page, rank);
             }
         }
 
-        private void SavePersonPageRank(Person person, Page page, int rank)
+        private void InsertNewPages(Page page, string htmlContent)
+        {
+            IEnumerable<string> pageUrls = Parser.GetPageUrls(htmlContent);
+
+            foreach (String url in pageUrls)
+            {
+                InsertUrl(page.Site, url);
+            }
+        }
+
+        private void InsertUrl(Site site, String url)
+        {
+            Page page = dataManager.Pages.GetAll().FirstOrDefault(p => p.URL == url);
+
+            if (page == null)
+            {
+                dataManager.Pages.Add(new Page()
+                {
+                    URL = url,
+                    Site = site,
+                    FoundDateTime = DateTime.Now
+                });
+            }
+        }
+
+        private void InsertPersonPageRank(Person person, Page page, int rank)
         {
             PersonPageRank personPageRank = dataManager.PersonPageRanks.GetById(person.Id, page.Id);
 
@@ -49,6 +81,14 @@ namespace Crawler.Engine
                     Page = page,
                     Rank = rank
                 });
+            }
+        }
+
+        private void FilterUrls(IEnumerable<String> urls, IEnumerable<String> disallowPaterns)
+        {
+            foreach (String disallowPattern in disallowPaterns)
+            {
+                urls = urls.Where(u => !Regex.IsMatch(u, disallowPattern));
             }
         }
 
