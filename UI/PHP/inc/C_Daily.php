9c8552ad1644168ef4a820266e0c1c43a37090dc
Дима <polishuk1003@gmail.com>
2016-02-08 23:11:45 +0300

    Контроллер для общей статистики
    Контроллер для общей статистики

---
+++ UI/PHP/inc/C_Daily.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class C_Daily extends C_Base
+{
+    private $daily;
+
+    public function __construct()
+    {
+        $this->daily = M_Daily::GetInstance();
+    }
+
+    public function action_index()
+    {
+        if(isset($_POST['sub']))
+            $daily = $this->daily->Get_Daily($_POST['persons'], $_POST['site'], $_POST['from_date'], $_POST['to_date']);
+        else
+            $daily = $this->daily->Get_Daily(1, 1);
+        $site = $this->daily->Get_Site();
+        $persons = $this->daily->Get_Persons();
+        $this->menu = $this->template("view/menu.php");
+        $this->content = $this->template("view/daily.php", ['daily' => $daily, 'site' => $site, 'persons' => $persons]);
+    }
+}
