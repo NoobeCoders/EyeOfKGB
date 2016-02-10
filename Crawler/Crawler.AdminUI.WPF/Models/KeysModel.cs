9595c95c41608ab77638ac45b777548bfd0675fa
Rammun <russslllan@mail.ru>
2016-02-10 14:14:45 +0300

    Добавил модель для ключей

--- Crawler/Crawler.AdminUI.WPF/Models/KeysModel.cs
+++ Crawler/Crawler.AdminUI.WPF/Models/KeysModel.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Crawler.AdminUI.WPF.Models
 {
-    public class KeysModel
+    public class KeysModel : DependencyObject
     {
+        public static DependencyProperty PeopleProperty;
+        public static DependencyProperty KeysProperty;
 
+        static KeysModel()
+        {
+            PeopleProperty = DependencyProperty.Register("People",
+                typeof(List<string>),
+                typeof(KeysModel),
+                new PropertyMetadata(new List<string>() { "выберите личность " }));
+
+            KeysProperty = DependencyProperty.Register("Keys",
+                typeof(List<string>),
+                typeof(KeysModel),
+                new PropertyMetadata(new List<string>()));
+        }
+
+        public List<string> People
+        {
+            get { return GetValue(PeopleProperty) as List<string>; }
+            set { SetValue(PeopleProperty, value); }
+        }
+
+        public List<string> Keys
+        {
+            get { return GetValue(KeysProperty) as List<string>; }
+            set { SetValue(KeysProperty, value); }
+        }
     }
 }
