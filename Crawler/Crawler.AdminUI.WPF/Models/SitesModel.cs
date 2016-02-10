308afd72e9aaca0da0098b5d0c3e9b4bf9215a55
Rammun <russslllan@mail.ru>
2016-02-10 14:34:59 +0300

    Добавил модель для сайтов

--- Crawler/Crawler.AdminUI.WPF/Models/SitesModel.cs
+++ Crawler/Crawler.AdminUI.WPF/Models/SitesModel.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Crawler.AdminUI.WPF.Models
 {
-    public class SitesModel
+    public class SitesModel : DependencyObject
     {
+        public static DependencyProperty SitesProperty;
+        public static DependencyProperty SelectedSiteProperty;
 
+        static SitesModel()
+        {
+            SitesProperty = DependencyProperty.Register("Sites",
+                                                        typeof(List<string>),
+                                                        typeof(SitesModel),
+                                                        new PropertyMetadata(new List<string>() { "выберите сайт " }));
+
+            SelectedSiteProperty = DependencyProperty.Register("SelectedSite",
+                                                        typeof(string),
+                                                        typeof(SitesModel),
+                                                        new PropertyMetadata(string.Empty));
+        }
+
+        public List<string> Sites
+        {
+            get { return GetValue(SitesProperty) as List<string>; }
+            set { SetValue(SitesProperty, value); }
+        }
+
+        public string SelectedSite
+        {
+            get { return GetValue(SelectedSiteProperty) as string; }
+            set { SetValue(SelectedSiteProperty, value); }
+        }
     }
 }
