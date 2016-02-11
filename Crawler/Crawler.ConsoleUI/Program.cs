c2e3fa402d03e3d09eb6fbcb793684ca8f199cdc
marcheneli <marcheneli@gmail.com>
2016-02-11 19:15:39 +0300

    Delete MySQL packages

--- Crawler/Crawler.ConsoleUI/Program.cs
+++ Crawler/Crawler.ConsoleUI/Program.cs
@@ -4,7 +4,6 @@ using Crawler.DAL;
 using Crawler.Domain.Entities;
 using Crawler.Domain.Interfaces;
 using Crawler.Engine;
-using MySql.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
