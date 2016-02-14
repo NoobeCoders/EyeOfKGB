2f81e3d8d635602dfd5c43cc9a8943a0d449b914
berluskuni <berluskuni@gmail.com>
2016-02-14 22:09:33 +0200

    Merge branch 'master' into rest-service.python-berl
    # Conflicts:
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB.xcodeproj/project.pbxproj
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB.xcodeproj/project.xcworkspace/xcuserdata/bu.xcuserdatad/UserInterfaceState.xcuserstate
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB.xcodeproj/xcuserdata/bu.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/Base.lproj/Main.storyboard
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticTableViewCell.h
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticViewController.h
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/GeneralStatisticViewController.m
    #	iOSApp/ObjectiveC/Eye Of KGB/Eye Of KGB/Info.plist

--- autorization/PHP/src/info/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Информация для базы и соль правда она не тут должна лежать наверное
-    $host = "mysql:host=localhost;dbname=eyeofkgb";
-    $user = "vin";
-    $pass = "admin";
-    $salt = 'testingforuserpassword';
-    ?>
